77a7a8e5a9ba726c2cdc6aa9318822127752c3d1
haruna <haru.4646@icloud.com>
2026-03-06 17:46:24 +0900

    予約完了ページ作成

--- src/main/java/controller/ThanksServlet.java
+++ src/main/java/controller/ThanksServlet.java
@@ -1,5 +1,49 @@
 package controller;
 
-public class ThanksServlet {
+import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.ContactBean;
+
+@WebServlet("/contact/thanks")
+
+public class ThanksServlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException{
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		//値の取得
+		String name = request.getParameter("name");
+		String email = request.getParameter("email_address");
+		String sex = request.getParameter("sex");
+		String []cates = request.getParameterValues("cates");
+		String pref = request.getParameter("pref");
+		String msg = request.getParameter("msg");
+				
+		// Beanに格納
+		ContactBean contact= new ContactBean();
+		contact.setName(name);
+		contact.setEmailAddress(email);
+		contact.setSex(sex);
+		contact.setCates(cates);
+		contact.setPref(pref);
+		contact.setMessage(msg);
+		
+		//jspに送る
+		request.setAttribute("contact", contact);
+		
+		// フォワード（画面に移す）
+    	RequestDispatcher dispatcher =
+    	        request.getRequestDispatcher("/WEB-INF/jsp/thanks.jsp");
+
+    	dispatcher.forward(request, response);
+	}
 }
--- src/main/webapp/WEB-INF/jsp/thanks.jsp
+++ src/main/webapp/WEB-INF/jsp/thanks.jsp
@@ -4,9 +4,42 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>送信完了</title>
 </head>
+
 <body>
+
+<h1>送信完了</h1>
+
+<p>お問い合わせありがとうございました。</p>
+
+<h2>送信内容</h2>
+
+<p>名前：${contact.name}</p>
+
+<p>メールアドレス：${contact.emailAddress}</p>
+
+<p>性別：${contact.sex}</p>
+
+<p>住まいエリア：${contact.pref}</p>
+
+<p>お問い合わせ内容：</p>
+<p>${contact.message}</p>
+
+<p>お問い合わせ種別：</p>
+<p>
+<%	String[] cates = (String[])request.getAttribute("cates");
+	if(cates != null){
+	    for(String c : cates){
+	        out.print(c + " ");
+	    }
+	}
+%>
+</p>
+
+<br>
+
+<a href="/contact-form/contact/input">入力画面に戻る</a>
 
 </body>
 </html>
